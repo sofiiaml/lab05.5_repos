7cf3b19f01d6780349bcbe7bbba799f43c6ce5a1
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-27 00:40:15 +0300

    lab5.5

---
+++ 05.5/05.5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+double bn(int b1, int q, int n) 
+{
+    if (n == 1) 
+        return b1;  
+    else 
+        return q * bn(b1, q, n - 1); 
+}
+
+double S(int b1, int q, int n) {
+    if (n == 1) {
+        return b1;  
+    }
+    else {
+        double t = bn(b1, q, n); 
+        return t + S(b1, q, n - 1); 
+    }
+}
+
+int main() 
+{
+    int b1, q, n;
+    cout << "b1= "; cin >> b1;
+    cout << "q= "; cin >> q;
+    cout << "n= "; cin >> n;
+    double t = bn(b1, q, n);
+    cout << "bn=" << t << endl;
+    double sum = S(b1, q, n);
+    cout << "S= " << sum << endl;
+
+    return 0;
+}
---
+++ UnitTest5.5/UnitTest5.5.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../05.5/05.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest55
+{
+	TEST_CLASS(UnitTest55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = bn(2,4,3);
+			Assert::AreEqual(t, 32, 0.000001);
+
+		}
+	};
+}
